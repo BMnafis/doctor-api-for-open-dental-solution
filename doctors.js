2069649e610cdae0e02e195fc4e2a662fbb0e4d5
BMnafis <mdnafis215@gmail.com>
2021-10-20 17:35:34 +0600

    first

---
+++ doctors.js
@@ -0,0 +1,38 @@
+const doctors = [
+    {
+        id: 1,
+        name: 'Richard D. Wright',
+        job_title: 'Pediatrician',
+        image: 'https://i.ibb.co/jz0GH9c/doctor1.jpg'
+    },
+    {
+        id: 2,
+        name: 'Aida J. Lowe',
+        job_title: 'Oral Surgery Specialist',
+        image: 'https://i.ibb.co/qRT80pK/doctor2.jpg'
+    },
+    {
+        id: 3,
+        name: 'Jamie Wright',
+        job_title: 'Periodontist',
+        image: 'https://i.ibb.co/cLRPdQN/doctor3.jpg'
+    },
+    {
+        id: 4,
+        name: 'Amanda Nickell',
+        job_title: 'Prosthodontics specialist',
+        image: 'https://i.ibb.co/3WpYz4t/doctor4.jpg'
+    },
+    {
+        id: 5,
+        name: 'Donald Weintraub',
+        job_title: 'Gynecologist',
+        image: 'https://i.ibb.co/7XTJPyd/doctor5.jpg'
+    },
+    {
+        id: 6,
+        name: 'Leon L. Cooper',
+        job_title: 'General Dentist',
+        image: 'https://i.ibb.co/F89H3Q3/doctor6.jpg'
+    }
+]
